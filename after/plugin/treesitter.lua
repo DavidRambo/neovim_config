d092b91013d015f88f74802abe867a71673e8864
David Rambo <davrambo@gmail.com>
2022-11-07 07:29:43 -0800

    Disable treesitter indent for python.

--- after/plugin/treesitter.lua
+++ after/plugin/treesitter.lua
@@ -1,25 +1,28 @@
-require('nvim-treesitter.configs').setup {
-    highlight = {
-        enable = true,
-    },
-    indent = { enable = true, },
-    -- A list of parser names
-    ensure_installed = {
-        "c",
-        "python",
-        "lua",
-        "bash",
-        "vim",
-        "dockerfile",
-        "html",
-        "json",
-        "java",
-        "javascript",
-        "markdown",
-        "scheme",
-        "gitignore",
-        "bash",
-    },
+require("nvim-treesitter.configs").setup({
+	highlight = {
+		enable = true,
+	},
+	indent = {
+		enable = true,
+		disable = { "python" },
+	},
+	-- A list of parser names
+	ensure_installed = {
+		"c",
+		"python",
+		"lua",
+		"bash",
+		"vim",
+		"dockerfile",
+		"html",
+		"json",
+		"java",
+		"javascript",
+		"markdown",
+		"scheme",
+		"gitignore",
+		"bash",
+	},
 
-    sync_install = false
-}
+	sync_install = false,
+})
