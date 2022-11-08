f9cf84a6df9dc5212d0fb20cb732db4d4591cf12
David Rambo <davidrambo@mailfence.com>
2022-11-07 17:08:57 -0800

    Hide null-ls code action prompt

--- after/plugin/lspsaga.lua
+++ after/plugin/lspsaga.lua
@@ -1,14 +1,19 @@
-local saga_status, saga = pcall(require, 'lspsaga')
+local saga_status, saga = pcall(require, "lspsaga")
 if not saga_status then
-    return
+	return
 end
 
 saga.init_lsp_saga({
-    move_in_saga = { prev = "<C-p>", next = "<C-n>" },
-    finder_action_keys = {
-        open = "<CR>",
-    },
-    definition_action_keys = {
-        edit = "<CR",
-    },
+	-- The below removes the lightbulb, but throws an error.
+	-- code_action_prompt = {
+	-- enable = false,
+	-- },
+	code_action_icon = "",
+	move_in_saga = { prev = "<C-p>", next = "<C-n>" },
+	finder_action_keys = {
+		open = "<CR>",
+	},
+	definition_action_keys = {
+		edit = "<CR",
+	},
 })
