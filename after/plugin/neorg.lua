a43b3e6d604319c18b5500db1a78660a709169d9
David Rambo <davrambo@gmail.com>
2022-12-17 21:16:45 -0800

    Add dropbox neorg workspace.

--- after/plugin/neorg.lua
+++ after/plugin/neorg.lua
@@ -7,6 +7,22 @@ neorg.setup({
 	load = {
 		["core.defaults"] = {},
 		["core.norg.concealer"] = {},
-		["core.keybinds"] = {},
+		["core.norg.manoeuvre"] = {},
+		["core.norg.dirman"] = {
+			config = {
+				workspaces = {
+					dropbox = "~/Dropbox (Maestral)/neorg/",
+				},
+			},
+		},
+		["core.keybinds"] = {
+			config = {
+				hook = function(keybinds)
+					-- Mappings for moving items up and down
+					keybinds.map("norg", "n", "M-j", "core.norg.manoeuvre.move_item_down")
+					keybinds.map("norg", "n", "M-k", "core.norg.manoeuvre.move_item_up")
+				end,
+			},
+		},
 	},
 })
