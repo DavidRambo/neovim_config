1bc195c605ae8e30b6d03264d189ed419e622fde
David Rambo <davrambo@gmail.com>
2022-12-21 15:36:25 -0800

    Add file finder func that defaults to git_files

--- lua/user/tele.lua
+++ lua/user/tele.lua
@@ -1,8 +1,9 @@
 local M = {}
 
+local themes = require("telescope.themes")
+
 -- Edit neovim config
 M.edit_nvim_conf = function()
-	-- local opts = themes.get_ivy = {}
 	require("telescope.builtin").git_files({
 		shorten_path = true,
 		cwd = "~/.config/nvim/",
@@ -14,6 +15,24 @@ M.edit_nvim_conf = function()
 			preview_width = 0.75,
 		},
 	})
+end
+
+-- Fall back to find_files if no .git directory.
+-- Source: https://github.com/nvim-telescope/telescope.nvim/wiki/Configuration-Recipes
+M.file_finder = function()
+	local opts = themes.get_ivy({
+		shorten_path = true,
+		layout_strategy = "horizontal",
+		layout_options = {
+			preview_width = 0.75,
+		},
+	})
+	vim.fn.system("git rev-parse --is-inside-work-tree")
+	if vim.v.shell_error == 0 then
+		require("telescope.builtin").git_files(opts)
+	else
+		require("telescope.builtin").find_files(opts)
+	end
 end
 
 return M
