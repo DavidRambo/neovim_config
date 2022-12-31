b891a54e1836bc4c2c57d00f012d454318a1147f
David Rambo <davidrambo@mailfence.com>
2022-12-31 12:20:02 -0800

    Configure Comment.nvim

--- after/plugin/comment.lua
+++ after/plugin/comment.lua
@@ -3,4 +3,30 @@ if not status then
 	return
 end
 
-comment.setup()
+comment.setup({
+	ignore = "^$",
+	toggler = {
+		line = "<leader>cc",
+		block = "<leader>cb",
+	},
+	opleader = {
+		line = "<leader>c",
+		block = "<leader>b",
+	},
+	extra = {
+		-- Add comment on the line above
+		above = "<leader>cO",
+		-- Add comment on the line below
+		below = "<leader>co",
+		-- Add comment at the end of line
+		eol = "<leader>ca",
+	},
+	-- Enable keybindings
+	-- Note: If given `false` then the plugin won't create any mappings
+	mappings = {
+		-- Operator-pending mapping; `gcc` `gbc` `gc[count]{motion}` `gb[count]{motion}`
+		basic = true,
+		-- Extra mapping; `gco`, `gcO`, `gcA`
+		extra = true,
+	},
+})
--- after/plugin/which-key.lua
+++ after/plugin/which-key.lua
@@ -11,6 +11,11 @@ wk.register({
 	-- 		name = "Hop",
 	-- 	},
 	-- },
+	["<leader>c"] = { -- These don't show up because "<leader>c" enters operation-pending
+		name = "[C]omment",
+		c = { "[C]omment Line" },
+		b = { "[C]omment [B]lock" },
+	},
 	["<leader>d"] = {
 		name = "diagnostics",
 		f = "Filter to errors only",
