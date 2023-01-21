114ecc495e36e19c5f6594f801a7557578609767
David Rambo <davrambo@gmail.com>
2023-01-21 10:58:13 -0800

    Swap in black for autopep8

--- after/plugin/mason.lua
+++ after/plugin/mason.lua
@@ -1,39 +1,39 @@
 -- Mason for handling LSP installation
-local mason_status, mason = pcall(require, 'mason')
+local mason_status, mason = pcall(require, "mason")
 if not mason_status then
-    return
+	return
 end
 
 mason.setup()
 
-local mason_lspconfig_status, mason_lspconfig = pcall(require, 'mason-lspconfig')
+local mason_lspconfig_status, mason_lspconfig = pcall(require, "mason-lspconfig")
 if not mason_lspconfig_status then
-    return
+	return
 end
 
 local mason_null_ls_status, mason_null_ls = pcall(require, "mason-null-ls")
 if not mason_null_ls_status then
-    return
+	return
 end
 
 mason_lspconfig.setup({
-    ensure_installed = {
-        "html",
-        "cssls",
-        "sumneko_lua",
-        "pylsp",
-        "pyright",
-        "clangd",
-        "sqlls",
-        "jdtls",
-    }
+	ensure_installed = {
+		"html",
+		"cssls",
+		"sumneko_lua",
+		"pylsp",
+		"pyright",
+		"black",
+		"clangd",
+		"sqlls",
+		"jdtls",
+	},
 })
 
 mason_null_ls.setup({
-    ensure_installed = {
-        "stylua",
-        "autopep8",
-        "clang_format",
-        "pylint",
-    }
+	ensure_installed = {
+		"stylua",
+		"clang_format",
+		"pylint",
+	},
 })
