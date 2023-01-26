9872527387d5a71ce3ff43973ece953790ccc3ba
David Rambo <davrambo@gmail.com>
2023-01-26 13:23:35 -0800

    Add poetry pyenv function for pyright lsp.

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -58,6 +58,40 @@ lspconfig["clangd"].setup({
 	capabilities = capabilities,
 })
 
+local util = require("lspconfig/util")
+
+local path = util.path
+
+local function get_python_path(workspace)
+	-- Use activated virtualenv.
+	if vim.env.VIRTUAL_ENV then
+		return path.join(vim.env.VIRTUAL_ENV, "bin", "python")
+	end
+
+	-- Find and use virtualenv via poetry in workspace directory.
+	local match = vim.fn.glob(path.join(workspace, "poetry.lock"))
+	if match ~= "" then
+		local venv = vim.fn.trim(vim.fn.system("poetry env info -p"))
+		-- Or, if using nested poetry environments (requires poetry 1.3.0):
+		-- local venv = vim.fn.trim(vim.fn.system('poetry --directory ' .. workspace .. ' env info -p'))
+		return path.join(venv, "bin", "python")
+	end
+
+	-- Fallback to system Python.
+	return vim.fn.exepath("python3") or vim.fn.exepath("python") or "python"
+end
+
+require("lspconfig").pyright.setup({
+	on_attach = function()
+		require("lsp_signature").on_attach({
+			hint_enable = false,
+		})
+	end,
+	on_init = function(client)
+		client.config.settings.python.pythonPath = get_python_path(client.config.root_dir)
+	end,
+})
+
 lspconfig.pyright.before_init = function(params, config)
 	local Path = require("plenary.path")
 	local venv = Path:new((config.root_dir:gsub("/", Path.path.sep)), ".venv")
