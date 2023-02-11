ec5127b6648dfb5ea80f09e50ef78b441a52e174
David Rambo <davidrambo@mailfence.com>
2023-02-10 21:15:44 -0800

    Comment out unmapping 's' in neogit.

--- after/plugin/neogit.lua
+++ after/plugin/neogit.lua
@@ -76,7 +76,7 @@ neogit.setup({
 			-- Adds a mapping with "B" as key that does the "BranchPopup" command
 			["B"] = "BranchPopup",
 			-- Removes the default mapping of "s"
-			["s"] = "",
+			-- ["s"] = "",
 		},
 	},
 })
